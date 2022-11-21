12a04b3e81c40b6b237638193c199b6845cf5f44
tunasmoothie <tunasmoothie@gmail.com>
2022-11-21 11:18:09 +0800

    added env.sh

---
+++ env.sh
@@ -0,0 +1,13 @@
+# User File Locations
+XSA_FILE_PATH=./top_wrapper.xsa
+
+
+# 2021.1 Toolset Paths
+VIVADO_PATH_2021_1=/tools/Xilinx/Vivado/2021.1
+VITIS_PATH_2021_1=/tools/Xilinx/Vitis/2021.1
+PLNXSDK_PATH_2021_1=~/Documents/plnxSDK2021.1
+
+
+${VIVADO_PATH_2021_1}/settings64.sh
+${VITIS_PATH_2021_1}/settings64.sh
+${PLNXSDK_PATH_2021_1}/settings.sh
