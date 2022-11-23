7bfe6b3c439b2cf3a8b5eeb6aeea4afc438badb2
tunasmoothie <tunasmoothie@gmail.com>
2022-11-23 15:24:01 +0800

    added prebuilt

---
+++ prebuild_project/hardware/xilinx-k26-starterkit-2021.1/project_1.gen/sources_1/bd/project_1/hdl/project_1_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2021.1 (lin64) Build 3247384 Thu Jun 10 19:36:07 MDT 2021
+//Date        : Wed Nov 16 15:25:51 2022
+//Host        : eric-pc running 64-bit Ubuntu 20.04.5 LTS
+//Command     : generate_target project_1_wrapper.bd
+//Design      : project_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module project_1_wrapper
+   ();
+
+
+  project_1 project_1_i
+       ();
+endmodule
